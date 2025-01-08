701cbbaa73f94e67ac48040ec9364cc1f3b0e592
Laassel <lassel.giovanni@gmail.com>
2025-01-08 15:34:08 +0100

    ADD movies Controller.js index function to get all movies from database

---
+++ server/controllers/moviesController.js
@@ -0,0 +1,23 @@
+//importo la connesione dal database
+const { json } = require('express');
+const connection = require('../data/db'); 
+
+// funzione per ottenre la lista di tutti i film 
+
+const index = (req, res) => {
+    // eseguo la query per ottenere tutti i film 
+    connection.query('SELECT * FROM movies', (err, results) => {
+        if(err) {
+            console.error('Errore nella query', err);
+            return res.status(500).send('Errore nel recupero dei film');
+        }
+        
+        if(results.length === 0) {
+            return res.status(404).send('Nessun film trovato.');
+        }
+
+        res.status(200).json(results);
+    });
+};
+
+module.exports = index
