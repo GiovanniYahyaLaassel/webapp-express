e84d5c736682c715f72817548eef9f7bc6170139
Laassel <lassel.giovanni@gmail.com>
2025-01-09 13:07:20 +0100

    ADD:   the show function to get each individual movie with its reviews from the database.

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -19,6 +19,43 @@ const index = (req, res) => {
     });
 };
 
-module.exports = {
-    index
+
+// Funzione per ottenere ogni singolo film con le sue recensioni 
+
+const show = (req, res) => {
+    // ottengo l'id del film dalla richiesta 
+    const movieId = parseInt(req.params.id);
+    
+    // eseguo la query per ottenere
+    connection.query('SELECT * FROM movies WHERE id = ?', [movieId],  (err, movieResult) => {
+        if(err) {
+            console.error('Errore nella query', err);
+            return res.status(500).send('Errore nel recupero dei film');
+        }
+
+        if (movieResult.length === 0) {
+            return res.status(404).send('Film non trovato');
+        }
+
+         // eseguo una query per ottenere le recensioni a quel film
+         connection.query('SELECT * FROM reviews WHERE movie_id = ?', [movieId], (err, reviewsResult) => {
+            if (err) {
+                console.error('Errore nella query delle recensioni:', err);
+                return res.status(500).send('Errore nel recupero delle recensioni');
+            }
+
+             // film e recensioni in formato JSON
+             res.status(200).json({
+                movie: movieResult[0],
+                reviews: reviewsResult  
+            });
+            
+        });
+    });
 };
+
+module.exports = {
+    index,
+    show
+};
+
--- server/routes/moviesRoutes.js
+++ server/routes/moviesRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/moviesController');
 
+// Rotta per ottenere la lista di tutti i film
 router.get('/', moviesController.index);
+
+// Rotta per avere i dettagli di un singolo film con le recensioni
+router.get('/:id', moviesController.show);
+
 
 module.exports = router
